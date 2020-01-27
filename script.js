0fa4f0bd2d553c72bc9f2ba16f8da43dfceeebf8
Olivia Letendart <letendartolivia@gmail.com>
2020-01-27 09:54:55 +0100

    comlete function checkSlide, create console.log + loop forEach

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ function debounce(func, wait = 20, immediate = true) {
 const sliderImages = document.querySelectorAll(".slide-in");
 
 function checkSlide(e) {
-  console.count(e);
+  console.log(window.scrollY);
+  sliderImages.forEach(slideImage => {
+    // const slideInAt =
+  });
 }
 
 window.addEventListener("scroll", debounce(checkSlide));
